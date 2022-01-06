fcb0ecb7eb4634d7d8b14125821ae4383838a95b
xEyad <eyiadhisham@yahoo.com>
2022-01-06 23:39:31 +0200

    better portfolio

--- src/app/section-selector/section-selector.component.ts
+++ src/app/section-selector/section-selector.component.ts
@@ -9,7 +9,7 @@ export class SectionSelectorComponent implements OnInit {
   faCamera = faCamera;
   faPhone = faPhone;
   faUser = faUser;
-  _activeSelection:number = 0;
+  _activeSelection:number = 1;
   @Output() activeSelection = new EventEmitter<number>();
   constructor() { }
 
