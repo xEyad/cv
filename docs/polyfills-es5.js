9cff35141854cf63ed59b71b2f45e92d14ce2987
xEyad <eyiadhisham@yahoo.com>
2022-02-18 14:23:18 +0200

    new build

--- docs/polyfills-es5.js
+++ docs/polyfills-es5.js
@@ -20745,7 +20745,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
   /***/
   function _(module, exports, __webpack_require__) {
     __webpack_require__(
-    /*! K:\workshop\websites\simple-cv\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-polyfills.js */
+    /*! M:\websites\simple-cv\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-polyfills.js */
     "./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
 
     __webpack_require__(
@@ -20753,15 +20753,15 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
     "./node_modules/zone.js/dist/zone-legacy.js");
 
     __webpack_require__(
-    /*! K:\workshop\websites\simple-cv\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */
+    /*! M:\websites\simple-cv\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */
     "./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
     __webpack_require__(
-    /*! K:\workshop\websites\simple-cv\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-jit-polyfills.js */
+    /*! M:\websites\simple-cv\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-jit-polyfills.js */
     "./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-jit-polyfills.js");
 
     module.exports = __webpack_require__(
-    /*! K:\workshop\websites\simple-cv\src\polyfills.ts */
+    /*! M:\websites\simple-cv\src\polyfills.ts */
     "./src/polyfills.ts");
     /***/
   }
