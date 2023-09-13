d0b2aaf8d6fbfc4ab6817549c7a8bdeed3523879
Eyad <eyiadhisham@yahoo.com>
2023-09-13 16:12:38 +0300

    set selection to local storage

--- src/app/section-selector/section-selector.component.ts
+++ src/app/section-selector/section-selector.component.ts
@@ -1,26 +1,25 @@
-import { Component, OnInit, Output,EventEmitter } from '@angular/core';
-import { faCamera, faUser,faPhone} from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { faCamera, faUser, faPhone } from "@fortawesome/free-solid-svg-icons";
 @Component({
-  selector: 'app-section-selector',
-  templateUrl: './section-selector.component.html',
-  styleUrls: ['./section-selector.component.scss']
+  selector: "app-section-selector",
+  templateUrl: "./section-selector.component.html",
+  styleUrls: ["./section-selector.component.scss"],
 })
 export class SectionSelectorComponent implements OnInit {
   faCamera = faCamera;
   faPhone = faPhone;
   faUser = faUser;
-  _activeSelection:number = 0;
+  _activeSelection: number = 0;
   @Output() activeSelection = new EventEmitter<number>();
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    this.setSelection(this._activeSelection);
+    this.setSelection(parseInt(localStorage.getItem("activeSelection")) || 0);
   }
 
-  setSelection(val)
-  {
+  setSelection(val) {
     this._activeSelection = val;
     this.activeSelection.emit(this._activeSelection);
+    localStorage.setItem("activeSelection", this._activeSelection.toString());
   }
-
 }
